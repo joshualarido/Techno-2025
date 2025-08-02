68f9b070475f614b72348a78a2dd0b0d9ad8fa21
Ariq <ariqhakim97@gmail.com>
2025-08-02 17:44:56 +0700

    feat: Menambahkan halaman Events dengan background dan komponen scroll

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar"; 
+import bgevents from "../assets/bgevents.png";
+
+import ScrollBackground from "../components/Eventtitle.jsx";
+
+
 const Events = () => {
     return (
-        <>
-            <h1 className="text-center font-bold underline text-5xl">Events</h1>
+        <>  
+        <div
+            className="flex flex-col bg-no-repeat bg-center bg-cover min-h-screen w-screen"
+            style={{ backgroundImage: `url(${bgevents})` }}
+        >
+            <Navbar />
+
+            {/* Konten Halaman Events */}
+            <div className="flex flex-col items-center justify-center w-full py-10">
+                <ScrollBackground text="Events" /> 
+                
+                {/* Jika Anda memiliki konten lain di halaman Events, letakkan di sini */}
+            </div>
+            
+        </div>
+            
+            
+            
         </>
     );
 }
