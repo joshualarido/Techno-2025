fd990accf949e0dc62819b630f608a455ae9c581
joshualarido <joshualarido@gmail.com>
2025-08-05 20:52:11 +0700

    git merge conflict resolve

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,26 +8,15 @@ function Footer() {
   return (
     <>
    
-    <footer className="bg-gradient-to-b from-sky-400/90 via-blue-800/80 to-blue-800/80 flex flex-col gap-12 py-12">
-
-      <div className="flex flex-row max-lg:flex-row gap-12 max-lg:gap-2 justify-center items-center">
-        <img src= {logoTechno} alt="Logo Techno" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
-        <img src= {logoBinus} alt="Logo Binus" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
-        <img src={logoHimti} alt="Logo HIMTI" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
-      </div>
-      
-      <div className="container mx-auto text-center text-white font-alice">
-        <p className="text-xl">© 2025 Himpunan Mahasiswa Teknik Informatika (HIMTI) BINUS UNIVERSITY.</p>
-        <p className="text-xl">© All Rights Reserved.</p>
-      </div>
-
-    </footer>
-
+      <footer className="bg-gradient-to-b from-sky-400/90 via-blue-800/80 to-blue-800/80 flex flex-col gap-12 py-12">
+        <div className="flex flex-row max-lg:flex-row gap-12 max-lg:gap-2 justify-center items-center">
+          <img src= {logoTechno} alt="Logo Techno" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
+          <img src= {logoBinus} alt="Logo Binus" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
+          <img src={logoHimti} alt="Logo HIMTI" className="h-[128px] max-lg:h-auto max-lg:w-1/5" />
+        </div>
+        
         <div className="container mx-auto text-center text-white font-alice">
-          <p className="text-xl">
-            © 2025 Himpunan Mahasiswa Teknik Informatika (HIMTI) BINUS
-            UNIVERSITY.
-          </p>
+          <p className="text-xl">© 2025 Himpunan Mahasiswa Teknik Informatika (HIMTI) BINUS UNIVERSITY.</p>
           <p className="text-xl">© All Rights Reserved.</p>
         </div>
       </footer>
